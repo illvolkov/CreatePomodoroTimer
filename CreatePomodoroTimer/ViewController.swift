ec780e5a75222ffb42e6bf2e526a7ed08259f6be
Ilya Volkov <illvolkov@bk.ru>
2022-01-27 07:52:00 +0500

    Create startPauseButton and states for button.

--- CreatePomodoroTimer/ViewController.swift
+++ CreatePomodoroTimer/ViewController.swift
@@ -13,6 +13,19 @@ class ViewController: UIViewController {
     var isWorkTime = true
     //Флаг для конфигурации кнопки
     var isStarted = false
+    
+    //MARK: - Elements
+
+    private lazy var startPauseButton: UIButton = {
+        let button = UIButton(type: .system)
+        
+        let buttonConfig = UIImage.SymbolConfiguration(pointSize: 100, weight: .thin)
+        button.setImage(UIImage(systemName: "play", withConfiguration: buttonConfig), for: .normal)
+        button.tintColor = UIColor(red: 241/255.0, green: 137/255.0, blue: 126/255.0, alpha: 1)
+        button.addTarget(self, action: #selector(statesButton), for: .touchUpInside)
+
+        return button
+    }()
 
     //MARK: - Lifecycle
 
@@ -33,5 +46,18 @@ class ViewController: UIViewController {
         
     }
 
+    //MARK: - Create functions
+
+    @objc func statesButton() {
+        let buttonConfig = UIImage.SymbolConfiguration(pointSize: 100, weight: .thin)
+
+        if isStarted == false {
+            startPauseButton.setImage(UIImage(systemName: "pause", withConfiguration: buttonConfig), for: .normal)
+            isStarted = true
+        } else {
+            startPauseButton.setImage(UIImage(systemName: "play", withConfiguration: buttonConfig), for: .normal)
+            isStarted = false
+        }
+    }
 }
 
