7dff22492fe1a281f898d361443dad8b027f47c4
Ilya Volkov <illvolkov@bk.ru>
2022-01-27 07:46:48 +0500

    add method setupHierarchy and SetupLayout

--- CreatePomodoroTimer/ViewController.swift
+++ CreatePomodoroTimer/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: - Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupHierarchy()
+        setupLayout()
     }
-
+    
+    //MARK: - Settings
+    
+    private func setupHierarchy() {
+        
+    }
+    
+    private func setupLayout() {
+        
+    }
 
 }
 
